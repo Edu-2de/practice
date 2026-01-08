91bab4a2cd7eb0b1b2ecf99e5b5f590d2a7e6630
Eduardo <edupaim1712@gmail.com>
2026-01-08 17:27:28 -0300

    Add routes for greeting and budget calculation; create OrcamentoController

--- PHP/00-meu-primeiro-laravel/routes/web.php
+++ PHP/00-meu-primeiro-laravel/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ola', function () {
+    return "Meu primeiro sistema Laravel!";
+});
+
+Route::get('/orcamento/{valor}/{horas}', function ($valor, $horas) {
+    return "Orcamento deu: " . $valor * $horas;
+});
