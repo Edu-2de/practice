ab3a49efe4722c385a7175c10c4fb13b975e25b9
Eduardo <edupaim1712@gmail.com>
2026-01-08 17:03:10 -0300

    Reorganize require statements and enhance output for Projeto and ProjetoEspecial instances

--- PHP/Projeto1/index.php
+++ PHP/Projeto1/index.php
@@ -2,10 +2,31 @@
 
 namespace App\Financeiro;
 
-require_once "src/Consultoria.php";
-require_once "src/Contrato.php";
-require_once "src/Formatador.php";
-require_once "src/Logavel.php";
 require_once "src/Orcavel.php";
+require_once "src/Logavel.php";
 require_once "src/Projeto.php";
 require_once "src/ProjetoEspecial.php";
+require_once "src/Formatador.php";
+require_once "src/Contrato.php";
+require_once "src/Consultoria.php";
+
+$app = new Projeto('app de Financas', 100.0, 300.5);
+$site = new ProjetoEspecial('site de Financas', 200.0, 40.3);
+echo  $app->mostrarInfos() . " ficou em um total de " . Formatador::moeda($app->calcularOrcamento())  . "\n";
+echo  $site->mostrarInfos() . " ficou em um total de " . Formatador::moeda($site->calcularOrcamento()) . "\n";
+
+try {
+    $app->setValorHora(-50.0);
+    echo "Sucesso!\n";
+} catch (\Exception $e) {
+    echo "Erro!: " . $e->getMessage();
+}
+
+echo  $app->mostrarInfos() . " ficou em um total de " . Formatador::moeda($app->calcularOrcamento()) . "\n";
+
+$contratoApp = new Contrato();
+echo $contratoApp->criarContrato($app) . "\n";
+
+$consultoria = new Consultoria();
+$contratoConsultoria = new Contrato();
+echo $contratoConsultoria->criarContrato($consultoria) . "\n";
