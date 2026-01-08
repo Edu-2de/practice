f50595f109224f1031ea79ccc1a5ec7fe20017dd
Eduardo <edupaim1712@gmail.com>
2026-01-08 16:56:43 -0300

    Refactor Projeto class: enhance budget calculation and logging functionality

--- PHP/Projeto1/src/Projeto.php
+++ PHP/Projeto1/src/Projeto.php
@@ -1,3 +1,45 @@
 <?php
 
 namespace App\Financeiro;
+
+class Projeto implements Orcavel
+{
+    public string $nome;
+    public float $hora;
+    private float $valor_hora;
+
+    use Logavel;
+
+    public function __construct($nome_recebido, $hora_recebida, $valor_hora_recebido)
+    {
+        $this->nome = $nome_recebido;
+        $this->hora = $hora_recebida;
+        $this->valor_hora = $valor_hora_recebido;
+    }
+
+    public function calcularOrcamento(): float
+    {
+        $this->registrarLog("Calculando orçamento...");
+        return $this->hora * $this->valor_hora;
+    }
+
+    public function mostrarInfos(): string
+    {
+        return "O projeto $this->nome tem: $this->hora horas, com valor/hora de: $this->valor_hora\n";
+    }
+
+    public function setValorHora(float $novoValor)
+    {
+        if ($novoValor <= 0) {
+            throw new \Exception("O valor da hora não pode ser zero ou negativo.\n");
+        } else {
+            $this->valor_hora = $novoValor;
+            return "Novo valor definido!\n";
+        }
+    }
+
+    public function getValorHora(): float
+    {
+        return $this->valor_hora;
+    }
+}
