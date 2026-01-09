e6d3e3b7e59281b14e22c9197dd49e31170f597b
Eduardo <edupaim1712@gmail.com>
2026-01-09 17:35:09 -0300

    Add relationship method for Cliente in Orcamento model and create test client route

--- PHP/00-meu-primeiro-laravel/app/Models/Orcamento.php
+++ PHP/00-meu-primeiro-laravel/app/Models/Orcamento.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Cliente;
 
 class Orcamento extends Model
 {
     protected $fillable = ['cliente', 'valor_hora', 'total_horas', 'valor_final'];
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
 }
--- PHP/00-meu-primeiro-laravel/routes/web.php
+++ PHP/00-meu-primeiro-laravel/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrcamentoController;
 use Symfony\Component\Routing\Router;
+use App\Models\Cliente;
 
 Route::get('/novo-orcamento', [OrcamentoController::class, 'mostrarFormulario']);
 
@@ -15,3 +16,11 @@ Route::delete('/orcamento/{id}', [OrcamentoController::class, 'excluir']);
 Route::get('/orcamento/{id}/editar', [OrcamentoController::class, 'editar']);
 
 Route::put('/orcamento/{id}', [OrcamentoController::class, 'update']);
+
+Route::get('/criar-cliente-teste', function () {
+    Cliente::create([
+        'nome' => 'Empresa ABC Ltda',
+        'telefone' => '(11) 99999-8888'
+    ]);
+    return "Cliente de teste criado com sucesso!";
+});
