44224e1073fb9f0039eb94fcbe0895a443cadae4
Eduardo <edupaim1712@gmail.com>
2026-01-13 14:57:28 -0300

    feat: Add CartItem model and migration for cart items table

--- PHP/01-sistema-moderno/database/seeders/DatabaseSeeder.php
+++ PHP/01-sistema-moderno/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \App\Models\User::factory()->create([
+            'name' => 'Eduardo Paim',
+            'email' => 'eduardo@teste.com',
+            'password' => bcrypt('password'),
         ]);
+
+        \App\Models\Product::factory(20)->create();
     }
 }
